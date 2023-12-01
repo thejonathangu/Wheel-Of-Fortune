752c1f309f69558f2e7dd4eaaee11b1091b8b5bb
thejonathangu <149909500+thejonathangu@users.noreply.github.com>
2023-11-30 22:09:30 -0500

    fixed board, have answer choices

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import random
 import time
 from collections import Counter
 import math
+import string # string.ascii_.lowercase
 
 class player():
     def __init__(self, name):
@@ -72,7 +73,7 @@ class computerPlayer1(player):
         # Get the most popular letter after the last guessed letter
         guess = self.commonNextLetter(revealedLetters)
 
-        # Check if the  guessed letter is not already guessed
+        # Check if the guessed letter is not already guessed
         while guess in self.guessedLetters:
             guess = self.commonNextLetter(revealedLetters)
 
@@ -114,7 +115,7 @@ def onAppStart(app):
     app.gameOver = False
     app.maxScore = 0
     app.rows = 5
-    app.cols = 15
+    app.cols = 16
     app.boardLeft = 40
     app.boardTop = 45
     app.boardWidth = 600
@@ -122,30 +123,32 @@ def onAppStart(app):
     app.cellBorderWidth = 1
     app.board = [([None] * app.cols) for row in range(app.rows)]
     app.spinWheel = False
-    app.prizes = {'Lose a Turn':'white', 
-                  '$800':'red', 
-                  '$550':'darkViolet',
-                  '$650':'pink',
-                  '$500':'green',
-                  '$900':'orange',
-                  'BANKRUPT':'black',
-                  '$600':'gold',
-                  '$400':'blue',
-                  '$300':'yellow',
-                  '$1000':'indigo',
-                  '$450':'cyan',
-                  'Lose a Turn':'white', 
-                  '$800':'red', 
-                  '$550':'darkViolet',
-                  '$650':'pink',
-                  '$500':'green',
-                  '$900':'orange',
-                  'BANKRUPT':'black',
-                  '$600':'gold',
-                  '$400':'blue',
-                  '$300':'yellow',
-                  '$1000':'indigo',
-                  '$450':'cyan'}
+    app.filledInAnswer = False
+    app.currentPrize = 0
+#    app.prizes = {'Lose a Turn':'white', 
+#                  '$800':'red', 
+#                  '$550':'darkViolet',
+#                  '$650':'pink',
+#                  '$500':'green',
+#                  '$900':'orange',
+#                  'BANKRUPT':'black',
+#                  '$600':'gold',
+#                  '$400':'blue',
+#                  '$300':'yellow',
+#                  '$1000':'indigo',
+#                  '$450':'cyan',
+#                  'Lose a Turn':'white', 
+#                  '$800':'red', 
+#                  '$550':'darkViolet',
+#                  '$650':'pink',
+#                  '$500':'green',
+#                  '$900':'orange',
+#                  'BANKRUPT':'black',
+#                  '$600':'gold',
+#                  '$400':'blue',
+#                  '$300':'yellow',
+#                  '$1000':'indigo',
+#                  '$450':'cyan'}
     app.humanPlayer = humanPlayer('You')
     app.computerPlayer1 = computerPlayer1('Computer 1', difficulty = 'easy')
 
@@ -157,7 +160,7 @@ def restartGame(app):
     app.gameOver = False
     app.maxScore = 0
     app.rows = 5
-    app.cols = 15
+    app.cols = 16
     app.boardLeft = 40
     app.boardTop = 45
     app.boardWidth = 600
@@ -199,8 +202,10 @@ def restartGame(app):
                        'laborer','spring','relate','angle','monstrous','hierarchy','owe','promotion',
                        'grimace','norm','loot','captain','lawyer','bubble']
     app.answer = random.choice(app.randomWords)
+    app.guessedLetters = set()
     app.filledInAnswer = False
-    app.spinSpeed = 800
+    app.spinSpeed = random.randint(10,30)*200
+    app.spinFriction = 100
 
     for playerObj in app.players:
         playerObj.money = 0
@@ -231,7 +236,8 @@ def drawCell(app, row, col, color, opac):
     cellLeft, cellTop = getCellLeftTop(app, row, col)
     cellWidth, cellHeight = getCellSize(app)
     drawRect(cellLeft, cellTop, cellWidth, cellHeight,
-             fill=color, border='black',
+             #fill=color, border='black',
+             fill='green', border='black',
              borderWidth=app.cellBorderWidth, opacity = opac)
 
 #gets the location of each cell's locations
@@ -241,11 +247,39 @@ def getCellLeftTop(app, row, col):
     cellTop = app.boardTop + row * cellHeight
     return (cellLeft, cellTop)
 
+#draw red arrow
+def drawRedArrow(app):
+    drawStar(app.width/2, app.height/5+20, 20, 3, rotateAngle = 180, fill='red', border='red')
+
+#draw spin button
+def drawSpinButton(app):
+    drawCircle(app.width/2, app.height/2, 20, fill='lightgray', border='red')
+    drawLabel('SPIN', app.width/2, app.height/2)
+
+#draw new game button
+def drawNewGameButton(app):
+    drawRect(app.width/2-60, app.height - 60, 120, 22 , fill='lightgray', border='green')
+    drawLabel('New Game?', app.width/2, app.height - 50, size = 20, bold=True)
+
+#draw solve game button
+def drawSolveGameButton(app):
+    drawRect(app.width/2-72, app.height - 60, 140, 22, fill='lightgray', border='green')
+    drawLabel('Solve Game?', app.width/2, app.height - 50, size = 20, bold=True)
+
+
 #gets the size of each cell
 def getCellSize(app):
     cellWidth = app.boardWidth / app.cols
     cellHeight = app.boardHeight / app.rows
     return (cellWidth, cellHeight)
+
+def draw26Letters(app):
+    for idx , letter in enumerate(string.ascii_lowercase):
+        (x,y) = getCellLeftTop(app, 30 + int(idx/9) , 5+idx%9 )
+        w,h = getCellSize(app)
+        drawRect(x-w/2,y-h/2,w,h,fill='darkgreen', opacity= 90)
+        drawLabel(letter.upper(),x,y, size=20, fill='gold', bold =True)
+
 
 def onKeyPress(app, key):
     if key == 's':
@@ -254,6 +288,63 @@ def onKeyPress(app, key):
         app.userInput.append(key)
     if key == 'enter':
         checkIfInAnswer(app,app.userInput)
+
+
+def getSelectedLetter(app, mouseX, mouseY):
+    labels = [s for s in app.group if (isinstance(s, Label) and s.contains(mouseX, mouseY))]
+    letters = [ l.value for l in labels if (l.value.lower() in list(string.ascii_lowercase)) ] 
+    return letters[0] if len(letters) > 0 else None 
+
+def getNewGame(app, mouseX, mouseY):
+    labels = [s for s in app.group if (isinstance(s, Label) and s.contains(mouseX, mouseY))]
+    letters = [ l.value for l in labels if (l.value  == "New Game?")]
+    return letters[0] if len(letters) > 0 else None 
+
+def getSolveGame(app, mouseX, mouseY):
+    labels = [s for s in app.group if (isinstance(s, Label) and s.contains(mouseX, mouseY))]
+    letters = [ l.value for l in labels if (l.value  == "Solve Game?")]
+    return letters[0] if len(letters) > 0 else None 
+
+
+def isSolved(app):
+    for letter in app.answer:
+        if letter.upper() not in app.guessedLetters:
+            return False
+    return True
+
+def onMousePress(app, mouseX, mouseY):
+    if mouseX <= app.width/2 + 20 and mouseX >= app.width/2 - 20:
+        if mouseY <= app.height/2 + 20 and mouseY >= app.height/2 - 20:
+            app.spinSpeed = random.randint(800, 1600)
+            app.spinWheel = True
+            #restartGame(app)
+            return
+
+    #check input
+    letter = getSelectedLetter(app, mouseX, mouseY)
+    if letter is not None:
+        print(f'the selected letter is : {letter}')
+        app.guessedLetters.add(letter)
+        print(app.guessedLetters)
+        print(app.answer, app.guessedLetters)
+        if isSolved(app):
+            app.gameOver = True
+
+    #check input
+    solveGame = getSolveGame(app, mouseX, mouseY)
+    if solveGame is not None:
+        guessedInput = app.getTextInput('Your guess:')
+        for letter in guessedInput:
+            app.guessedLetters.add(letter.upper())
+        if isSolved(app):
+            app.gameOver = True
+
+
+    newGame = getNewGame(app, mouseX, mouseY)
+    if newGame is not None:
+        app.gameOver = False
+        restartGame(app)
+
 
 def checkIfInAnswer(app, userInput):
     if app.gameOver:
@@ -280,21 +371,32 @@ def drawAnswerPhrase(app):
     cellWidth, cellHeight = getCellSize(app)
 
     for i in range(answerLength):
-        x, y = getCellLeftTop(app, 2, i)
+        x, y = getCellLeftTop(app, 2, i+1)
         drawRect(x, y, cellWidth, cellHeight, fill='lightgrey', border='black', borderWidth=app.cellBorderWidth)
-        if app.answer[i] in app.userInput:
-            drawLabel(app.answer[i], x + cellWidth / 2, y + cellHeight / 2, fontSize=20, bold=True)
+        if app.answer[i].upper() in app.guessedLetters:
+            drawLabel(app.answer[i].upper(), x + cellWidth / 2, y + cellHeight / 2, size=20, bold=True)
 
-    app.filledInAnswer = True
+    #app.filledInAnswer = True
         
 def onStep(app):
     if app.spinWheel == True:
         if app.spinSpeed>=0:
             takeStep(app)
-            app.spinSpeed -=2
+            app.spinSpeed -=app.spinFriction
+            #print (list(app.prizes.keys())[0])
         else:
             app.spinWheel = False
+            print (list(app.prizes.keys())[0], list(app.prizes.values())[0])
+            selected_label = getSelectedLabel(app)
+            print (f'Spin to :{selected_label.value}')
+            print (f'{app.answer}')
+            #app.gameOver = True
     
+def getSelectedLabel(app):
+    labels = [s for s in app.group if isinstance(s, Label)]
+    selected_label = min (labels , key = lambda x : abs((x.rotateAngle+360) %360 - 90))
+
+    return selected_label
 
 def takeStep(app):
     rotateList(app)
@@ -343,14 +445,18 @@ def drawPlayerMoney(app):
     playerObj = app.humanPlayer
     playerName = playerObj.name
     playerMoney = playerObj.money
-    drawLabel(f"{playerName}: ${playerMoney}", 0,app.height-20,)
+    (x,y) = getCellLeftTop(app, 40  , 3 )
+    w,h = getCellSize(app)
+    drawLabel(f"{playerName}: ${playerMoney}", x,  app.height-20, bold=True )
 
 def drawComputerMoney(app):
 
     playerObj = app.computerPlayer1
     playerName = playerObj.name
     playerMoney = playerObj.money
-    drawLabel(f"{playerName}: ${playerMoney}", 100, app.height-20)
+    (x,y) = getCellLeftTop(app, 40  , 15 )
+    w,h = getCellSize(app)
+    drawLabel(f"{playerName}: ${playerMoney}", x,  app.height-20, bold=True )
 
 def gameOverScreen(app):
     drawLabel("Game Over!", app.width // 2, app.height // 2, size=50, bold=True)
@@ -361,9 +467,16 @@ def redrawAll(app):
         drawBoard(app)
         drawBoardBorder(app)
         drawWheel(app)
+        drawRedArrow(app)
+        drawSpinButton(app)
         drawPlayerMoney(app)
+        drawComputerMoney(app)
+        drawAnswerPhrase(app)
+        draw26Letters(app)
+        drawSolveGameButton(app)
     else:
         gameOverScreen(app)
+        drawNewGameButton(app)
 
 
 def main():
