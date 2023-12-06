919fe2b17044b054be1b4b387e3b2b9d4b11fc8e
thejonathangu <149909500+thejonathangu@users.noreply.github.com>
2023-12-06 15:42:57 -0500

    Update button.py

--- button.py
+++ button.py
@@ -1,5 +1,6 @@
 from cmu_graphics import *
 
+#The button class
 class Button():
     def __init__(self, x, y, width, height, label, size, active = True):
         self.width = width
@@ -17,14 +18,17 @@ class Button():
         self.centerX = x
         self.centerY = y
 
+    #Draws the buttons
     def draw(self):
         opacity = 100 if self.active else 30
         drawRect(self.left, self.top, self.width, self.height, fill='goldenrod', border = 'gray', opacity = opacity)
         drawLabel(self.text, self.x, self.y , size = self.size, opacity = opacity)
 
+    #Sets button as pressed or not
     def setActive(self, active):
         self.active = active
 
+    #checks if mouse press hits within box
     def contains(self, mouseX, mouseY):
         if not self.active:
             return None
